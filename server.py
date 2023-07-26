f75e458382e5b1123c6538b1f57e05acbda15b9b
yatin <yatinchaudhari13@gmail.com>
2023-07-26 16:17:17 +0530

    Version 4 Updated..!

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-  return '<h1 style="color: lavender">Hey Buddy....! Welcome to my Flask App Version 3 #####!</h1>'
+  return '<h1 style="color: orange">Hey Buddy....! Welcome to my Flask App Version 3 #####!</h1>'
 
 
 app.run(port=3333, host="0.0.0.0", debug=True)
